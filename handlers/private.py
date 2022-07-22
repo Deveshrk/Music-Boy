2d7a6dc39e71d2dd2dc38ed41f62d5743a783aa8
Deveshrk <109092437+Deveshrk@users.noreply.github.com>
2022-07-22 16:39:05 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -13,8 +13,8 @@ async def start_(client: Client, message: Message):
         photo=f"https://te.legra.ph/file/3162314db1d339632cd51.jpg",
         caption=f"""**𝚃𝙷𝙸𝚂 𝙸𝚂 𝙰𝙽 𝙰𝙳𝚅𝙰𝙽𝙲𝙴𝙳 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 𝙼𝚄𝚂𝙸𝙲 𝙱𝙾𝚃 𝚁𝚄𝙽𝚂 𝙾𝙽 𝙿𝚁𝙸𝚅𝙰𝚃𝙴 𝚅𝙿𝚂 𝚆𝙸𝚃𝙷 𝙷𝙸𝙶𝙷 𝚀𝚄𝙰𝙻𝙸𝚃𝚈 𝙼𝚄𝚂𝙸𝙲 𝙰𝙽𝙳 𝚂𝚄𝙿𝙴𝚁 𝚂𝙿𝙴𝙴𝙳.𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙼𝙰𝙺𝙴 𝙼𝙴 𝙰𝙳𝙼𝙸𝙽 𝚃𝙷𝙴𝙽 𝚂𝙴𝙰𝚁𝙲𝙷 𝚈𝙾𝚄𝚁 𝙵𝙰𝚅𝙾𝚁𝙸𝚃𝙴 𝚂𝙾𝙽𝙶𝚂 𝚆𝙸𝚃𝙷 𝙲𝙾𝙼𝙼𝙰𝙽𝙳...//
 
-┣⪼ 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 :- [𝐀𝐒𝐆𝐀𝐑𝐃𝐅𝐀𝐌𝐈𝐋𝐘](https://t.me/NJ_AJ_WOULD)
-┣⪼ 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 :- [𝐀𝐒𝐆𝐀𝐑𝐃 𝐅𝐀𝐌𝐈𝐋𝐘](https://t.me/NJ_AJ_WOULD)
+┣⪼ 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 :- [𝐀𝐒𝐆𝐀𝐑𝐃𝐅𝐀𝐌𝐈𝐋𝐘](https://t.me/NJ_AJ_WORLD)
+┣⪼ 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 :- [𝐀𝐒𝐆𝐀𝐑𝐃 𝐅𝐀𝐌𝐈𝐋𝐘](https://t.me/NJ_AJ_WORLD)
 
  
 𝙿𝙻𝙴𝙰𝚂𝙴 𝚂𝙷𝙰𝚁𝙴 𝙰𝙽𝙳 𝚂𝚄𝙿𝙿𝙾𝚁𝚃..//**""",
@@ -22,7 +22,7 @@ async def start_(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✅𝐀𝐒𝐆𝐀𝐑𝐃 𝐅𝐀𝐌𝐈𝐋𝐘✅", url=f"https://t.me/NJ_AJ_WOULD")
+                        "✅𝐀𝐒𝐆𝐀𝐑𝐃 𝐅𝐀𝐌𝐈𝐋𝐘✅", url=f"https://t.me/NJ_AJ_WORLD")
                 ]
                 
            ]
